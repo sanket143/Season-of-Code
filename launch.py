19bd1566f8c1b4d19eb801fbab971b6b1aa75dab
root <chaudharisanket2000@gmail.com>
2018-01-16 08:02:23 -0800

    Added Tkinter Graphics

--- launch.py
+++ launch.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import re
 import config
 from bs4 import BeautifulSoup as bs
@@ -5,24 +6,39 @@ import requests
 import ast
 import string
 import tkinter as tk
-def innerHTML(element):
-    return element.decode_contents(formatter="html")
+info = ""
+def start():
+	global info
+	repos = open("repo.json","r")
+	JSONdata = ast.literal_eval(repos.read())
+	repos.close()
 
-repos = open("repo.json","r")
-JSONdata = ast.literal_eval(repos.read())
-repos.close()
+	for obj in JSONdata:
+		link = "https://github.com/"+obj["mentor"]+"/"+obj["project"]+"/issues?q=is%3Apr+is%3Aclosed"
+		r = requests.get(link)
+		soup = bs(r.content,"html.parser")
+		contributions = soup.find_all("div",{"class":"float-left col-9 p-2 lh-condensed"})
+		print ("Mentor:",obj["mentor"],"\t",obj["project"])
+		for cont in contributions:
+			cont = re.sub(r'\s+'," ",str(cont))
+			cont = (re.sub(r'<(.*?)>',"",str(cont)))
+			if obj["mentor"] not in str(cont):
+				info = info + str(cont) + "\n"
+		content.config(text=str(info))
+		info += "\n"
 
-for repositories in JSONdata:
-	link = "https://github.com/"+repositories+"/"+JSONdata[repositories]+"/issues?q=is%3Apr+is%3Aclosed"
-	#/harshvasoya008/Game_of_Cards/issues?q=is%3Apr+is%3Aclosed
-	r = requests.get(link)
-	soup = bs(r.content,"html.parser")
-	contributions = soup.find_all("div",{"class":"float-left col-9 p-2 lh-condensed"})
-	for cont in contributions:
-		dummy = innerHTML(cont)
-		print string.replace(dummy,r'<(.*?)>',"")
+window = tk.Tk()
 
+window.title("Season of Code (Evaluator)")
 
+window.geometry("800x600")
 
-	
+# LABEL
+title = tk.Label(text="Welcome to DAWoC and welcome to SoC Evaluator",font=("Times New Roman", 20))
+title.grid(column=1,row=0)
 
+content = tk.Label(text="Please Wait")
+content.grid(column=1,row=2)
+window.after(1000,start)
+
+window.mainloop()
