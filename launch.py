1041bb73752d26f222328bfa33c96a4464c0ae6e
root <chaudharisanket2000@gmail.com>
2018-01-16 11:36:03 -0800

    modified script

--- launch.py
+++ launch.py
@@ -8,6 +8,7 @@ import ast
 from Tkinter import *
 info = ""
 def start():
+	print ("Evalution going on..")
 	global info
 	track = 0
 	repos = open("repo.json","r")
@@ -17,7 +18,11 @@ def start():
 	for obj in JSONdata:
 		track+=1
 		link = "https://github.com/"+obj["mentor"]+"/"+obj["project"]+"/issues?q=is%3Apr+is%3Aclosed"
-		r = requests.get(link)
+		try:
+			r = requests.get(link)
+		except:
+			print ("Check your Internet Connection.")
+			exit()
 		soup = bs(r.content,"html.parser")
 		contributions = soup.find_all("div",{"class":"float-left col-9 p-2 lh-condensed"})
 		if track is len(JSONdata):
@@ -25,7 +30,7 @@ def start():
 		print(str(progress)+"%..........")
 		for cont in contributions:
 			cont = re.sub(r'\s+'," ",str(cont))
-			cont = (re.sub(r'<(.*?)>',"",str(cont)))
+			cont = re.sub(r'<(.*?)>',"",str(cont))
 			if obj["mentor"] not in str(cont):
 				info = info + str(cont) + "\n"
 		content.config(text=str(info))
@@ -44,7 +49,7 @@ title = tk.Label(text="Welcome to DAWoC and welcome to SoC Evaluator",font=("Tim
 title.grid(column=1,row=0)
 
 content = tk.Label(text="Please Wait")
-content.grid(column=1,row=2)
+content.grid(column=0,row=2)
 window.after(1000,start)
 
 window.mainloop()
